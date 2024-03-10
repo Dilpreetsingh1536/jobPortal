aaf9fa03f3ca576e70115891fb1b334925ab7fdc
eric-khedia <erickhedia.main@gmail.com>
2024-03-10 19:44:48 -0400

    final

--- app.js
+++ app.js
@@ -20,57 +20,65 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.listen(3001, () => {
-  console.log("Server is listening at port 3001");
+    console.log("Server is listening at port 3001");
 });
 
 app.set("views", [
-  path.join(__dirname, "views"),
-  path.join(__dirname, "views", "user"),
-  path.join(__dirname, "views", "employer"),
-  path.join(__dirname, "views", "job"),
-  path.join(__dirname, "views", "admin"),
-  path.join(__dirname, "views", "partials")
+    path.join(__dirname, "views"),
+    path.join(__dirname, "views", "user"),
+    path.join(__dirname, "views", "employer"),
+    path.join(__dirname, "views", "job"),
+    path.join(__dirname, "views", "admin"),
+    path.join(__dirname, "views", "partials")
 ]);
 
 /* SESSION */
 const uri = "mongodb+srv://dilpreet1999:Singh1536@cluster0.4g4xjah.mongodb.net/?retryWrites=true&w=majority";
 const accountSessionStore = MongoStore.create({
-  mongoUrl: uri,
-  dbName: "career_connect_session",
-  collectionName: "sessions",
+    mongoUrl: uri,
+    dbName: "career_connect_session",
+    collectionName: "sessions",
 });
 
 app.use(session({
-  secret: "A secret key to sign the cookie",
-  resave: false,
-  saveUninitialized: false,
-  store: accountSessionStore,
+    secret: "A secret key to sign the cookie",
+    resave: false,
+    saveUninitialized: false,
+    store: accountSessionStore,
 }));
 
 
 /* HOME */
 app.get("/home", (req, res) => {
-  const user = req.session.user;
-  const employer = req.session.employer;
-  const admin = req.session.admin;
-  res.render("home", { user, admin, employer });
+    const user = req.session.user;
+    const employer = req.session.employer;
+    const admin = req.session.admin;
+    res.render("home", { user, admin, employer });
 });
 
 /*User Search*/
 app.get('/userSearch', (req, res) => {
-  const user = req.session.user;
-  const employer = req.session.employer;
-  const admin = req.session.admin;
-  res.render("userSearch", { user, employer, admin });
+    const user = req.session.user;
+    const employer = req.session.employer;
+    const admin = req.session.admin;
+    res.render("userSearch", { user, employer, admin });
 })
 
 /*Emp Search*/
 app.get('/empSearch', (req, res) => {
-  const user = req.session.user;
-  const employer = req.session.employer;
-  const admin = req.session.admin;
-  res.render("empSearch", { user, employer, admin });
+    const user = req.session.user;
+    const employer = req.session.employer;
+    const admin = req.session.admin;
+    res.render("empSearch", { user, employer, admin });
 })
+
+/*Abous us*/
+app.get('/aboutUs', (req, res) => {
+    const user = req.session.user;
+    const employer = req.session.employer;
+    const admin = req.session.admin;
+    res.render('aboutUs', { user, employer, admin });
+});
 
 
 
@@ -78,5 +86,3 @@ app.use("/", userRoutes);
 app.use("/", empRoutes);
 app.use("/", adminRoutes);
 app.use("/", jobRoutes);
-
-
