43f8d0d87bf07f719177f415ba0a94c0471b7006
Dilpreetsingh1536 <151109865+Dilpreetsingh1536@users.noreply.github.com>
2024-04-13 19:21:20 -0400

    Changes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.listen(3002, () => {
+app.listen(3001, () => {
     console.log("Server is listening at port 3001");
 });
 
